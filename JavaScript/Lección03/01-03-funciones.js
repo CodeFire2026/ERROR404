71fa2ff0211d04f10e6234b912d114f042c97385
Zamira <zamiramontoro@gmail.com>
2026-08-31 22:51:05 -0300

    JS-LECCIÓN 3-VIDEO 4

--- JavaScript/Lección03/01-03-funciones.js
+++ JavaScript/Lección03/01-03-funciones.js
@@ -1,8 +1,23 @@
 miFuncion(8, 2); // Esto se le conoce como hosting
 
 function miFuncion(a,b){
-    console.log("Sumamos: "+ (a + b));
+    //console.log("Sumamos: "+ (a + b));
+    return a + b;
 }
 
 // Llamando la función
 miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
+
+// Declaramos una función de tipo expresión o anónima
+let x = function(a, b){ return a + b}; // Necesita cierre con punto y coma
+resultado = x(5, 6); // al llamarla se pone la variable y paréntesis
+console.log(resultado);
+
+
+// Funciones de tipo self y invoking
+(function(a,b){
+    console.log("Ejecutando la funcion: "+ (a + b));
+})(9, 6);
