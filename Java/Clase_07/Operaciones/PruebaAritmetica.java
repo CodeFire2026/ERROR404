030a7a52dba4f8a5c5d886f00ba2e9eb87c2129c
Pamela G. M. Cuello <pam.rodriguez3@gmail.com>
2026-09-23 21:17:46 -0300

    JV-Clase 07 - Prueba aritmética

--- Java/Clase_07/Operaciones/PruebaAritmetica.java
+++ Java/Clase_07/Operaciones/PruebaAritmetica.java
@@ -17,19 +17,32 @@ public class PruebaAritmetica {
         
         resultado = aritmetica1.sumarConArgemuntos(12,26);
         System.out.println("Resultado usando argumentos = " + resultado);
+        
+        //System.out.println("aritmetica2 = " + aritmetica2.a);
+        //System.out.println("aritmetica2 = " + aritmetica2.b);
+        //aritmetica1=null; nunca utilizar esto no se debe hacer
+        //System.gc(); metodo para limpiar residuos, es pesado, no utilizar
+        Persona persona = new Persona("Ariel", "Betancud");
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombre: "+persona.nombre);
+        System.out.println("Persona nombre: "+persona.apellido);
+                
+        //Modularidad creamos un nuevo metodo
     }
 
-    private static void miMetodo() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public static void miMetodo(){
+            //a = 10; //una variable esta limitada
+            System.out.println("Aqui hay un metodo");
+        }   
 }
+
 /*este constructor apunta a los atributos que hemos contruído en esta clase*/
 class Persona{
     /*creamos una clase default dentro de otro paquete*/
     String nombre;
     String apellido;
     
-    Persona(String nombre, String apellido){
+    Persona(String nombre, String apellido){ //constructor
         this.nombre = nombre;
         this.apellido = apellido; 
     }
